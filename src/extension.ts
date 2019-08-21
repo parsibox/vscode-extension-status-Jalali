9def03c4b391c36d69aadb25dcb0e09d29dbb4dc
محسن داوری <php.mohsen@gmail.com>
2019-08-21 10:20:44 +0430

    Add files via upload

--- src/extension.ts
+++ src/extension.ts
@@ -12,18 +12,15 @@ moment.loadPersian({usePersianDigits: true,dialect: 'persian-modern'});
 
 export function activate(context: vscode.ExtensionContext) {
 
-	// register a command that is invoked when the status bar
-	const myCommandId = 'extensions.mohsendavari.jalali-date-statusbar';
-
-
-	let disposable = vscode.commands.registerCommand('extensions.mohsendavari.jalali-date-statusbar', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-
-		let todayJalali = moment().format('ddd jDo jMMMM  jYYYY  ساعت  h و mm دقیقه');
-		vscode.window.showInformationMessage(`هم اکنون  ${todayJalali} میباشد`);
+	let myCommandId = 'extensions.mohsendavari.jalali-date-statusbar';
+	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(cfg => {
+        if (cfg.affectsConfiguration("extensions.mohsendavari.jalali-date-statusbar")) {
+				refreshProjects();
+        }
+	}));
 	
+	let disposable = vscode.commands.registerCommand(myCommandId, () => {
+		refreshProjects();	
 	});
 
 	context.subscriptions.push(disposable);
@@ -33,12 +30,17 @@ export function activate(context: vscode.ExtensionContext) {
 	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
 	myStatusBarItem.command = myCommandId;
 	context.subscriptions.push(myStatusBarItem);
-
-
 	// update status bar item once at start
 	updateStatusBarItem();
 }
-
+function refreshProjects(): void {
+	let todayJalali = moment().format('ddd jDo jMMMM  jYYYY  ساعت  h و mm دقیقه');
+	vscode.window.showInformationMessage(`هم اکنون  ${todayJalali} میباشد`);
+	const showProjectList = vscode.workspace.getConfiguration("mohsendavari.jalali-date-statusbar").get("showProjectList");
+	if( showProjectList){
+		vscode.commands.executeCommand('projectManager.listProjects');
+	}
+}
 function updateStatusBarItem(): void {
 	let todayJalali = moment().locale('fa').format('jYYYY/jM/jD');
 		myStatusBarItem.text = todayJalali;
